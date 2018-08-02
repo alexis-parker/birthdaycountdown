5c8444d3fd3dd7b3b08dec30fdde81b459565cd3
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-02 10:18:29 -0600

    added birhtday countdown title

--- Birthdaycountdown/src/components/app.js
+++ Birthdaycountdown/src/components/app.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-
-export default class App extends Component {
+ 
+ export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>DevCamp React Starter</h1>
-      </div>
-    );
-  }
-}
+      <div className="grid">
+        <h1 className="grid__title">Birthday Countdown</h1>
+         <div className="grid__skew-dark-one-box"></div>
+        <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-three"></div>
+ 
+         <div className="grid__skew-light-one"></div>
+         <div className="grid__skew-light-two"></div>
+         <div className="grid__skew-light-three-box"></div>
+       </div>
+     );
+   }
+ }
