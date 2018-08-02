479da17c0ffa920730ce8395026c8624ccdf7d1c
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-02 10:44:02 -0600

    placed dark skews and refactored SCSS

--- Birthdaycountdown/src/components/app.js
+++ Birthdaycountdown/src/components/app.js
@@ -4,14 +4,15 @@ import React, { Component } from 'react';
   render() {
     return (
       <div className="grid">
-        <h1 className="grid__title">Birthday Countdown</h1>
-         <div className="grid__skew-dark-one-box"></div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div className="grid__skew-dark-one-box"></div>
         <div className="grid__skew-dark-two"></div>
         <div className="grid__skew-dark-three"></div>
- 
          <div className="grid__skew-light-one"></div>
-         <div className="grid__skew-light-two"></div>
-         <div className="grid__skew-light-three-box"></div>
+        <div className="grid__skew-light-two"></div>
+        <div className="grid__skew-light-three-box"></div>
        </div>
      );
    }
